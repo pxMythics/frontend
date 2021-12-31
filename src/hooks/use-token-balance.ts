a5d90a40d4f589f69226a9cfcee6d09aabe4afb0
Gabriel Cartier <gcartier@samsao.co>
2021-12-31 16:07:57 -0500

    Removed logging

--- src/hooks/use-token-balance.ts
+++ src/hooks/use-token-balance.ts
@@ -1,13 +1,10 @@
 import { useContractCall, useEthers } from '@usedapp/core';
 import { Config } from 'config';
 import { useContractAbi } from 'hooks/use-contract-abi';
-import { useLogger } from 'provider/logger-provider';
 
 export const useTokenBalance = (): number | undefined => {
   const { account } = useEthers();
-  const logger = useLogger();
   const contractABI = useContractAbi();
-  logger.info(`fetching token balance for contract ${Config.contractAddress}`);
   const tokenBalance = useContractCall(
     account &&
       Config.contractAddress && {
