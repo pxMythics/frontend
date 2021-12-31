b86ec21597de5e906c50687669bfe3266122fe6a
Gabriel Cartier <gcartier@samsao.co>
2021-12-31 16:06:26 -0500

    Removed font test
    Added logo

--- src/page/main-page.tsx
+++ src/page/main-page.tsx
@@ -7,12 +7,7 @@ import styled from 'styled-components';
 export const MainPage: React.FunctionComponent = () => {
   return (
     <PageLayout>
-      <StyledSpan>{'custom font test'}</StyledSpan>
       <HeroSection />
     </PageLayout>
   );
 };
-
-const StyledSpan = styled(Span)`
-  font-family: 'Press Start 2P', sans-serif;
-`;
