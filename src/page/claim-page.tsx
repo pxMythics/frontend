12f823a497459066ccb132fb39465711d992ef90
Ryan Cook <ryancook12@gmail.com>
2022-04-06 18:51:20 -0400

    using mp3 from google cloud storage

--- src/page/claim-page.tsx
+++ src/page/claim-page.tsx
@@ -10,7 +10,6 @@ import { ClaimStep6 } from 'components/claim/claim-step6';
 import { ClaimStep7 } from 'components/claim/claim-step7';
 import { ClaimStep8 } from 'components/claim/claim-step8';
 import { ClaimStep9 } from 'components/claim/claim-step9';
-import music from 'assets/music.mp3';
 import React, { useCallback, useState, useEffect } from 'react';
 import { LocalStorageKey } from 'service/local-storage';
 import styled from 'styled-components';
@@ -24,7 +23,7 @@ export const ClaimPage: React.FunctionComponent = () => {
   const moveToNextStep = useCallback(() => {
     setStep(step + 1);
   }, [step]);
-  const audio = new Audio(music);
+  const audio = new Audio('https://storage.googleapis.com/pxmythics-audio/claim-audio.mp3');
   useEffect(() => {
     audio.play();
     return () => {
